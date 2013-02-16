78a45089574e1b84c14b0d2bbe454c2d421398cb
Albert Wang <albertyw@mit.edu>
2013-02-16 02:16:49 -0500

    Updated link.sh

--- link.sh
+++ link.sh
@@ -2,15 +2,10 @@
 
 dotfiles=$HOME/.dotfiles
 
-ln -s $dotfiles/ackrc $HOME/.ackrc
 ln -s $dotfiles/bashrc $HOME/.bashrc
 ln -s $dotfiles/bin $HOME/.bin
-ln -s $dotfiles/emacs.d $HOME/.emacs.d
 ln -s $dotfiles/git $HOME/.git
 ln -s $dotfiles/gitconfig $HOME/.gitconfig
 ln -s $dotfiles/gitignore $HOME/.gitignore
-ln -s $dotfiles/octaverc $HOME/.octaverc
-ln -s $dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s $dotfiles/wallpaper $HOME/.wallpaper
 ln -s $dotfiles/xinitrc $HOME/.xinitrc
-ln -s $dotfiles/Xmodmap $HOME/.Xmodmap
