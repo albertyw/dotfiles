3d3024092d2bb00154bbc44d59ddb37c4c019db0
Albert Wang <albertyw@mit.edu>
2013-02-28 14:44:22 -0500

    Added git grep

---
+++ bin/git-grep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+repos=( 'db' 'scripts' 'tools' 'common' 'ui' 'cam' );
+cd /home/albertyw/limsnew/
+for i in ${repos[@]}; do
+    echo -e "\033[44m**$i**\033[0m\n"
+    cd $i
+    echo -e "$(git grep --color --line-number $1)"
+    cd ..
+done
+cd -
