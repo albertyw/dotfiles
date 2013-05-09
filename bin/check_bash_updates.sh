48759db9f66e93264d116496f7182177cd9c32c4
Albert Wang <albertyw@mit.edu>
2013-05-08 21:14:44 -0400

    Moved repo update checker to its own file

---
+++ bin/check_bash_updates.sh
@@ -0,0 +1,12 @@
+# Check if there are updates to this dotfiles repo
+cd ~/.dotfiles
+git fetch
+git diff master..origin
+diffoutput=$(git diff HEAD..origin)
+if [ "$diffoutput" != "" ] ; then
+    echo 'YOUR DOTFILES ARE OUT OF DATE';
+fi
+changes=$(git diff)
+if [ "$changes" != "" ]; then
+    echo 'YOU HAVE UNCOMMITTED CHANGES';
+fi
