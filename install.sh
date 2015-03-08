4341c6f89002482f0e09e56aa296c05db97f8344
Albert Wang <albertyw@mit.edu>
2015-03-07 19:58:28 -0800

    Make install.sh also install iotop, iftop, htop, and finger

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install whois tree
+sudo apt-get install iotop iftop htop
+sudo apt-get install finger whois tree
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
