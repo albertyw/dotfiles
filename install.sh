624fb2a94fe2cea072c1dc474c1e3bb9beab6e23
Albert Wang <albert@cellabus.com>
2015-03-08 03:56:19 -0700

    Fix #7 - Install more things

--- install.sh
+++ install.sh
@@ -2,5 +2,15 @@
 
 sudo apt-get update
 sudo apt-get install iotop iftop htop
-sudo apt-get install finger whois tree
+sudo apt-get install finger whois tree traceroute
+sudo apt-get install vim
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# git
+sudo apt-add-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git
+
+# Python/pip/virtualenvwrapper
+curl https://bootstrap.pypa.io/get-pip.py | sudo python
+sudo pip install virtualenvwrapper
