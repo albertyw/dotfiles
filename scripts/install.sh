0627d07ccf00ed2c364210c3b035d0fc027afae4
Albert Wang <albertyw@mit.edu>
2015-03-09 01:17:14 +0000

    Also install zip

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install iotop iftop htop
 sudo apt-get install finger whois tree traceroute
-sudo apt-get install vim
+sudo apt-get install vim zip
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git
