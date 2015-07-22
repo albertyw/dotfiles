3eb026a84101dbc00debfb4675b8e038b02138d4
Albert Wang <albertyw@mit.edu>
2015-07-22 04:05:42 -0700

    Add status.sh

---
+++ scripts/status.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+sudo apt-get install lm-sensors smartmontools hddtemp discus sysstat
+
+echo "\033[44m TEMPERATURES \033[0m"
+sensors
+echo ""
+
+echo "\033[44m DISK HEALTH \033[0m"
+echo "SDA"
+sudo smartctl -a /dev/sda
+hddtemp /dev/sda
+echo ""
+
+echo "\033[44m DISK USAGE \033[0m"
+discus
+echo ""
+
+echo "\033[44m NETWORK \033[0m"
+ifconfig
+echo ""
+
+echo "\033[44m UPTIME/USERS \033[0m"
+w
+echo ""
+
+echo "\033[44m MEMORY \033[0m"
+free -m
+echo ""
+
+echo "\033[44m IOSTAT \033[0m"
+iostat -m
