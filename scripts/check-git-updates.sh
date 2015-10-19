6322268e3cc5a58e50676861b2a57e868caac3a1
Albert Wang <albertyw@mit.edu>
2015-10-18 23:09:31 -0700

    Also check for outdated ssh

--- scripts/check-git-updates.sh
+++ scripts/check-git-updates.sh
@@ -9,3 +9,17 @@ changes=$(git diff)
 if [ "$changes" != "" ]; then
     echo 'YOU HAVE UNCOMMITTED CHANGES';
 fi
+
+# Check if there are updates to ssh
+if [ -d ~/.ssh/.git ]; then
+    cd ~/.ssh
+    git fetch
+    diffoutput=$(git diff HEAD..origin)
+    if [ "$diffoutput" != "" ] ; then
+        echo 'YOUR SSH IS OUT OF DATE';
+    fi
+    changes=$(git diff)
+    if [ "$changes" != "" ]; then
+        echo 'YOU HAVE UNCOMMITTED SSH CHANGES';
+    fi
+fi
