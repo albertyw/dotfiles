54be54ce4a38bba12d65b18b7c18542d95dce045
Albert Wang <albert@cellabus.com>
2015-10-27 10:38:53 -0700

    Install ruby-dev when installing rvm

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,3 +26,4 @@ curl -sL https://raw.githubusercontent.com/brainsik/virtualenv-burrito/master/vi
 # RVM (takes a long time)
 # gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 # curl -sSL https://get.rvm.io | bash -s stable --ruby
+# sudo apt-get install ruby-dev # Fixes mkmf errors
