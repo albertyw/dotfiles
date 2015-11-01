b11ad04006f4fb9c56a8f20fc7ab3e30ceea260e
Albert Wang <albert@cellabus.com>
2015-11-01 00:53:37 -0700

    Install ruby dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -27,6 +27,7 @@ curl -sL https://raw.githubusercontent.com/brainsik/virtualenv-burrito/master/vi
 # gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 # curl -sSL https://get.rvm.io | bash -s stable --ruby
 # sudo apt-get install ruby-dev # Fixes mkmf errors
+# sudo apt-get install libgmp3-dev # Fixes installing json gem
 
 # Node.js
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
