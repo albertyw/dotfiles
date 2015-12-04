e4e587037b1a0756c7d4c67d69fd5b61fe5a7504
Albert Wang <albert@cellabus.com>
2015-12-03 22:36:06 -0800

    Fix #10 - Simplify scripts/link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,62 +1,38 @@
 #!/bin/sh
 
+set -e
+
 dotfiles=$HOME/.dotfiles/files/
 
-if [ -f $HOME/.atom ] && [ ! -L $HOME/.atom ] ; then
-    mv $HOME/.atom $HOME/.atom~
-fi
-ln -s $dotfiles/atom $HOME/.atom
+move () {
+    echo $1
+    if [ -L $HOME/.$1 ] ; then
+        return 0
+    fi
+    if [ -f $HOME/.$1 ] || [ -d $HOME/.$1 ] ; then
+        mv $HOME/.$1 $HOME/.$1~
+    fi
+    ln -s $dotfiles/$1 $HOME/.$1
+}
 
-if [ -f $HOME/.bash_profile ] && [ ! -L $HOME/.bash_profile ] ; then
-    mv $HOME/.bash_profile $HOME/.bash_profile~
-fi
-ln -s $dotfiles/bash_profile $HOME/.bash_profile
+move atom
 
-if [ -f $HOME/.bashrc ] && [ ! -L $HOME/.bashrc ] ; then
-    mv $HOME/.bashrc $HOME/.bashrc~
-fi
-ln -s $dotfiles/bashrc $HOME/.bashrc
+move bash_profile
 
-if [ -d $HOME/.git ] && [ ! -L $HOME/.git ]; then
-    mv $HOME/.git $HOME/.git~
-fi
-if [ ! -d $HOME/.git ]; then
-    ln -s $dotfiles/git $HOME/.git
-fi
+move bashrc
 
-if [ -f $HOME/.gitconfig ] && [ ! -L $HOME/.gitconfig ]; then
-    mv $HOME/.gitconfig $HOME/.gitconfig~
-fi
-ln -s $dotfiles/gitconfig $HOME/.gitconfig
+move git
 
-if [ -f $HOME/.gitignore ] && [ ! -L $HOME/.gitignore ]; then
-    mv $HOME/.gitignore $HOME/.gitignore~
-fi
-ln -s $dotfiles/gitignore $HOME/.gitignore
+move gitconfig
 
-if [ -f $HOME/.irbrc ] && [ ! -L $HOME/.irbrc ]; then
-    mv $HOME/.irbrc $HOME/.irbrc~
-fi
-ln -s $dotfiles/irbrc $HOME/.irbrc
+move gitignore
 
-if [ -f $HOME/.selected_editor ] && [ ! -L $HOME/.selected_editor ]; then
-    mv $HOME/.selected_editor $HOME/.selected_editor~
-fi
-ln -s $dotfiles/selected_editor $HOME/.selected_editor
+move irbrc
 
-if [ -f $HOME/.sudo_as_admin_successful ] && [ ! -L $HOME/.sudo_as_admin_successful ]; then
-    mv $HOME/.sudo_as_admin_successful $HOME/.sudo_as_admin_successful~
-fi
-ln -s $dotfiles/sudo_as_admin_successful $HOME/.sudo_as_admin_successful
+move selected_editor
 
-if [ -d $HOME/.vim ] && [ ! -L $HOME/.vim ]; then
-    mv $HOME/.vim $HOME/.vim~
-fi
-if [ ! -d $HOME/.vim ]; then
-    ln -s $dotfiles/vim $HOME/.vim
-fi
+move sudo_as_admin_successful
 
-if [ -f $HOME/.vimrc ] && [ ! -L $HOME/.vimrc ]; then
-    mv $HOME/.vimrc $HOME/.vimrc~
-fi
-ln -s $dotfiles/vimrc $HOME/.vimrc
+move vim
+
+move vimrc
