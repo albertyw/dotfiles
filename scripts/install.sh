5d14596920a1d2d61c7b0cfa237386e9eeaad6fc
Albert Wang <albertyw@mit.edu>
2016-03-27 14:02:12 -0700

    Add Glide

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,3 +40,9 @@ curl https://bootstrap.pypa.io/get-pip.py | sudo python2
 curl https://bootstrap.pypa.io/get-pip.py | sudo python3
 sudo pip2 install virtualenvwrapper
 sudo pip3 install virtualenvwrapper
+
+# Go Glide
+wget https://github.com/Masterminds/glide/releases/download/0.10.1/glide-0.10.1-linux-amd64.tar.gz
+tar xvf glide-0.10.1-linux-amd64.tar.gz
+mv linux-amd64/glide ~/.dotfiles/bin
+rm -r linux-amd64
