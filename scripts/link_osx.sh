73e928166b5a80a75ef632ae0b05f7f4f568e7e6
Albert Wang <albertyw@uber.com>
2016-05-23 01:41:43 -0700

    Fix #16 - Add iterm2 config

--- scripts/link_osx.sh
+++ scripts/link_osx.sh
@@ -4,3 +4,6 @@
 
 mkdir -p ~/Library/KeyBindings/
 ln -s ~/.dotfiles/files/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+
+# Set up iterm config
+ln -s ~/.dotfiles/files/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
