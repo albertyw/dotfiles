5c737c03c79a0383e1995a5661c28d707114429f
Albert Wang <albertyw@uber.com>
2016-05-23 01:48:08 -0700

    Revert "Fix plist file"
    This reverts commit ef7c8ff4e48fa6ad72ad081df4c196ac1d87b613.

--- scripts/link_osx.sh
+++ scripts/link_osx.sh
@@ -4,6 +4,3 @@
 
 mkdir -p ~/Library/KeyBindings/
 ln -s ~/.dotfiles/files/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
-
-# Set up iterm config
-ln -s ~/.dotfiles/files/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
