e56a1996cef39bf3f257d3c4ea7c7200b5c763c0
Albert Wang <albert@cellabus.com>
2016-07-30 23:15:31 -0700

    Refactor check-git-updates

--- scripts/check-git-updates.sh
+++ scripts/check-git-updates.sh
@@ -1,33 +1,28 @@
-# Check if there are updates to this dotfiles repo
-cd ~/.dotfiles
-git fetch
-diffoutput=$(git diff HEAD..FETCH_HEAD)
-if [ "$diffoutput" != "" ] ; then
-    echo 'YOUR DOTFILES ARE OUT OF DATE';
-fi
-changes=$(git diff)
-if [ "$changes" != "" ]; then
-    echo 'YOU HAVE UNCOMMITTED CHANGES';
-fi
-changes=$(git ls-files --others --exclude-standard)
-if [ "$changes" != "" ]; then
-    echo 'YOU HAVE UNCOMMITTED FILES';
-fi
-
-# Check if there are updates to ssh
-if [ -d ~/.ssh/.git ]; then
-    cd ~/.ssh
+check () {
     git fetch
-    diffoutput=$(git diff HEAD..FETCH_HEAD)
-    if [ "$diffoutput" != "" ] ; then
-        echo 'YOUR SSH IS OUT OF DATE';
+    
+    changes=$(git diff HEAD..FETCH_HEAD)
+    if [ "$changes" != "" ] ; then
+        echo 'YOUR DOTFILES ARE OUT OF DATE';
     fi
+    
     changes=$(git diff)
     if [ "$changes" != "" ]; then
-        echo 'YOU HAVE UNCOMMITTED SSH CHANGES';
+        echo 'YOU HAVE UNCOMMITTED CHANGES';
     fi
+
     changes=$(git ls-files --others --exclude-standard)
     if [ "$changes" != "" ]; then
         echo 'YOU HAVE UNCOMMITTED FILES';
     fi
+}
+
+# Check if there are updates to this dotfiles repo
+cd ~/.dotfiles
+check
+
+# Check if there are updates to ssh
+if [ -d ~/.ssh/.git ]; then
+    cd ~/.ssh
+    check
 fi
