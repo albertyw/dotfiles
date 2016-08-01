2f559a0ae3fe096238e1557f602ff6765bd940d9
Albert Wang <albertyw@mit.edu>
2016-07-31 17:04:27 -0700

    Add git commit size to git-size.sh

--- bin/git-size.sh
+++ bin/git-size.sh
@@ -3,3 +3,6 @@ git count-objects -vH | grep size-pack | cut -d' ' -f 2-3
 echo ''
 echo 'Total Local Storage'
 du . -hs | cut -f 1
+echo ''
+echo 'Top 20 Largest Commits'
+git-size-commit.pl | sort -nr | head -n 20
