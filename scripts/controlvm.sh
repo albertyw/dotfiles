a50a13f25767c8298592fb0b603483af1a4bea83
Albert Wang <albertyw@mit.edu>
2016-10-29 19:54:20 -0700

    Add status argument to controlvm.sh

--- scripts/controlvm.sh
+++ scripts/controlvm.sh
@@ -16,7 +16,15 @@ case "$1" in
         echo $DIR
         sshfs -o follow_symlinks -o reconnect -o cache=no $VM:/home/albertyw/ $DIR
         ;;
+    status)
+        VMS=`vboxmanage list runningvms | grep $VM || true`
+        if [[ -z $VMS ]]; then
+            echo "Not Running"
+        else
+            echo "Running"
+        fi
+        ;;
     *)
-        echo $"Usage: $0 {start|stop|mount}"
+        echo $"Usage: $0 {start|stop|mount|status}"
         exit 1
 esac
