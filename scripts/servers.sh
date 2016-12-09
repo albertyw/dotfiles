abc6a8588f12d1d0b36eacc7d989ce9f983081f6
Albert Wang <albertyw@mit.edu>
2016-12-08 22:36:31 -0800

    Add script that goes through servers and runs commands

---
+++ scripts/servers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This is a simple replacement for puppet
+
+LOCAL_MACHINES="cellabus personal"
+REMOTE_MACHINES="
+cellabus.com
+prod.cellabus.com
+statsonice.com
+staging.statsonice.com
+albertyw.com
+pharmadataassociates.com
+dendr.io
+staging.dendr.io
+test.dendr.io
+"
+
+hosts=""
+hosts="$hosts $LOCAL_MACHINES"
+hosts="$hosts $REMOTE_MACHINES"
+
+for server in $hosts; do
+    echo $server
+done
