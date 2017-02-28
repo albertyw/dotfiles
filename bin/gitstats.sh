433b65cc7e4efd70cd5d47692dbcedf199f662eb
Albert Wang <albertyw@mit.edu>
2017-02-28 01:37:15 -0800

    Fix #45 - Make gitstats.sh work on both linux and bsd

--- bin/gitstats.sh
+++ bin/gitstats.sh
@@ -5,8 +5,31 @@ git shortlog -sn
 
 echo ""
 echo "Users by line changes"
-git ls-files | xargs -n1 git blame --line-porcelain | sed -n 's/^author //p' | sort -f | uniq -ic | sort -nr
+git ls-files | \
+    xargs -n1 git blame --line-porcelain 2> /dev/null | \
+    sed -n 's/^author //p' | \
+    sort -f | \
+    uniq -ic | \
+    sort -nr
 
 echo ""
 echo "Users by existing lines"
-git ls-tree -r HEAD|sed -re 's/^.{53}//'|while read filename; do file "$filename"; done|grep -E ': .*text'|sed -r -e 's/: .*//'|while read filename; do git blame -w "$filename"; done|sed -r -e 's/.*\((.*)[0-9]{4}-[0-9]{2}-[0-9]{2} .*/\1/' -e 's/ +$//'|sort|uniq -c
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    regexsed="sed -r"
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    regexsed='sed -E'
+fi
+git ls-tree -r HEAD | \
+    $regexsed -e 's/^.{53}//' | \
+    while read filename; do
+        file "$filename";
+    done | \
+    grep -E ': .*text' | \
+    $regexsed -e 's/: .*//' | \
+    while read filename; do
+        git blame -w "$filename";
+    done | \
+    $regexsed -e 's/.*\((.*)[0-9]{4}-[0-9]{2}-[0-9]{2} .*/\1/' -e 's/ +$//' | \
+    sort | \
+    uniq -c
