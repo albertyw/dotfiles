586dbdaf650b476f3468d4c1581f5a9c305e581e
Albert Wang <albertyw@mit.edu>
2017-04-14 02:26:40 -0700

    Double quote dirdiff

--- bin/dirdiff.sh
+++ bin/dirdiff.sh
@@ -2,6 +2,6 @@
 # This script diffs the structure of two directories
 # without comparing file contents
 
-file1="$(find $1 | sed "s/${1//\//\\/}//g" | sort)"
-file2="$(find $2 | sed "s/${2//\//\\/}//g" | sort)"
+file1="$(find "$1" | sed "s/${1//\//\\/}//g" | sort)"
+file2="$(find "$2" | sed "s/${2//\//\\/}//g" | sort)"
 diff <(echo "$file1") <(echo "$file2")
