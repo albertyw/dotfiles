6be694f8e9c34a71674c5335cd4a7f7407615653
Albert Wang <git@albertyw.com>
2017-07-03 16:02:32 -0700

    Update hardware lookup script

--- bin/hardware.sh
+++ bin/hardware.sh
@@ -1,12 +1,29 @@
-head -n1 /etc/issue
-uname -a
+#!/bin/bash
+
+# Get Ubuntu version
+echo "UBUNTU VERSION"
+lsb_release -a
+echo ""
+
+echo "CPU"
 grep "model name" /proc/cpuinfo
+echo ""
+
+echo "MEMORY"
 grep MemTotal /proc/meminfo
 echo ""
+
+echo "DISK PARTITIONS"
 cat /proc/partitions
 echo ""
+
+echo "DRIVES"
 df -h
 echo ""
+
+echo "PCI DEVICES"
 lspci
 echo ""
+
+echo "USB DEVICES"
 lsusb
