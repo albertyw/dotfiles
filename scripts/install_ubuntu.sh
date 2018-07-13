3c5c22e64f0073a8ca54b8d914ab342f75e83fdc
Albert Wang <git@albertyw.com>
2018-07-13 01:58:48 -0700

    Remove special git installation commands for ubuntu

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -34,11 +34,6 @@ sudo dpkg-reconfigure --frontend noninteractive tzdata
 sudo locale-gen en_US en_US.UTF-8
 sudo dpkg-reconfigure --frontend noninteractive locales
 
-# git
-sudo apt-add-repository -y ppa:git-core/ppa
-sudo apt update
-sudo apt install git
-
 # RVM (takes a long time)
 # gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 # curl -sSL https://get.rvm.io | bash -s stable --ruby
