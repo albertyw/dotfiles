195f31ffe5c67b0c40a0160193fb542148299cf1
Albert Wang <git@albertyw.com>
2018-09-16 22:22:42 -0700

    Lint chmod-private-keys

--- scripts/chmod-private-keys.sh
+++ scripts/chmod-private-keys.sh
@@ -6,17 +6,17 @@ secure_secret_files () {
     fi
 }
 
-secretfiles=$(find ~/.ssh | grep id_.*$ | grep -vF .pub)
-secure_secret_files $secretfiles
+secretfiles=$(find ~/.ssh | grep "id_.*$" | grep -vF .pub)
+secure_secret_files "$secretfiles"
 
 secretfiles=$(find ~/.ssh | grep _rsa$)
-secure_secret_files $secretfiles
+secure_secret_files "$secretfiles"
 
 secretfiles=$(find ~/.ssh | grep .pem)
-secure_secret_files $secretfiles
+secure_secret_files "$secretfiles"
 
 secretfiles=$(find ~/.ssh | grep s3cfg)
-secure_secret_files $secretfiles
+secure_secret_files "$secretfiles"
 
 secretfiles=$(find ~/.ssh | grep p12)
-secure_secret_files $secretfiles
+secure_secret_files "$secretfiles"
