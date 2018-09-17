4c83c06a184eb20ae91fedcc193bca4aea205e6b
Albert Wang <git@albertyw.com>
2018-09-16 22:27:21 -0700

    Lint link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -5,19 +5,19 @@ set -e
 dotfiles=$HOME/.dotfiles/files/
 
 move () {
-    if [ -z $2 ] ; then
+    if [ -z "$2" ] ; then
         dest=$1
     else
         dest=$2
     fi
-    echo $1
-    if [ -L $HOME/.$dest ] ; then
+    echo "$1"
+    if [ -L "$HOME"/."$dest" ] ; then
         return 0
     fi
-    if [ -f $HOME/.$dest ] || [ -d $HOME/.$dest ] ; then
-        mv $HOME/.$dest $HOME/.$dest~
+    if [ -f $HOME/.$dest ] || [ -d "$HOME"/."$dest" ] ; then
+        mv "$HOME"/."$dest" "$HOME"/."$dest"~
     fi
-    ln -s $dotfiles/$1 $HOME/.$dest
+    ln -s "$dotfiles"/"$1" "$HOME"/."$dest"
 }
 
 git submodule init
@@ -34,7 +34,7 @@ move selected_editor
 move sudo_as_admin_successful
 move vim
 move vimrc
-if [[ `hostname` == *"uber"* ]] ; then
+if [[ $(hostname) == *"uber"* ]] ; then
     move gitconfig_uber gitconfig_local
     move bashrc_uber bashrc_local
 fi
