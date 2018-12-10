be13e3acb9649313ca1298d9ff0c879b7c9efa4b
Albert Wang <albertyw@uber.com>
2018-12-10 00:11:09 -0500

    Quote update-dotfiles to prevent parsing issues

--- scripts/update-dotfiles.sh
+++ scripts/update-dotfiles.sh
@@ -4,19 +4,19 @@ check () {
     git submodule --quiet init
     git submodule --quiet update --recursive
 
-    changes=$(git diff)
+    changes="$(git diff)"
     if [ "$changes" != "" ]; then
         echo 'YOU HAVE UNCOMMITTED CHANGES';
         exit
     fi
 
-    changes=$(git ls-files --others --exclude-standard)
+    changes="$(git ls-files --others --exclude-standard)"
     if [ "$changes" != "" ]; then
         echo 'YOU HAVE UNCOMMITTED FILES';
         exit
     fi
 
-    changes=$(git diff HEAD..FETCH_HEAD)
+    changes="$(git diff HEAD..FETCH_HEAD)"
     if [ "$changes" != "" ] ; then
         git pull --quiet > /dev/null 2>&1
         ~/.dotfiles/scripts/chmod-private-keys.sh
