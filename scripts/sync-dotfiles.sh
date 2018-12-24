187d3dd78e0ad6c2472d66d9f8fca9a049014616
Albert Wang <git@albertyw.com>
2018-12-24 09:59:05 +0800

    Refactor update dotfile submodules

--- scripts/sync-dotfiles.sh
+++ scripts/sync-dotfiles.sh
@@ -10,9 +10,6 @@ check_internet () {
 check () {
     git fetch --prune
 
-    git submodule --quiet init
-    git submodule --quiet update --recursive
-
     changes="$(git diff)"
     if [ "$changes" != "" ]; then
         echo 'YOU HAVE UNCOMMITTED CHANGES';
@@ -28,12 +25,11 @@ check () {
     changes="$(git diff HEAD..FETCH_HEAD)"
     if [ "$changes" != "" ] ; then
         git pull --quiet > /dev/null 2>&1
+        git submodule --quiet init
+        git submodule --quiet update --recursive
         ~/.dotfiles/scripts/chmod-private-keys.sh
         echo 'Dotfiles updated'
     fi
-
-    git submodule --quiet init
-    git submodule --quiet update --recursive
 }
 
 # Check if there are updates to this dotfiles repo
