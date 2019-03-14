5a3d6a362f213ccb2c5fca5115ce82b89b6e7e34
Albert Wang <git@albertyw.com>
2019-03-13 23:14:31 -0700

    Ignore test script linting itself

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ while read -r bashfile; do
     shellcheck "$bashfile"
 done <<< "$bashfiles"
 
-pythonfiles=$(git grep -El '#!.*python')
+pythonfiles=$(git grep -El '#!.*python' | grep -v "test.sh")
 
 while read -r pythonfile; do
     flake8 "$pythonfile"
