2fa5ce0723d593aaa59e63111867cd545361bbb4
Albert Wang <git@albertyw.com>
2019-03-22 23:14:53 -0700

    Also test bash config files

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,9 @@
 
 set -e
 
-bashfiles=$(git grep -El '#!.*bash')
+bashshebang=$(git grep -El '#!.*bash')
+bashfiles=$(git ls-files | grep bash)
+bashfiles="$bashshebang$IFS$bashfiles"
 
 while read -r bashfile; do
     shellcheck -e SC1090,SC1091 "$bashfile"
