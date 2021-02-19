03fd00a8bfa06bbcc1d91722f865afa925e25d9c
Albert Wang <git@albertyw.com>
2021-02-18 23:37:26 -0800

    Test script only for bash

--- scripts/test_bash.sh
+++ scripts/test_bash.sh
@@ -8,7 +8,6 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$BASEDIR/.." || exit 1
 
 
-echo "--- Run shellcheck"
 bashfiles=$(git grep -El '#!.*bash')
 bashfiles="$bashfiles$IFS$(git ls-files | grep bash)"
 bashfiles="$bashfiles$IFS$(git ls-files | grep -F .sh)"
@@ -17,19 +16,11 @@ shellcheckignores="SC1090,SC1091"
 
 while read -r bashfile; do
     echo "$bashfile"
-    # bash completion files are meant to be sourced instead of run so they 
+    # bash completion files are meant to be sourced instead of run so they
     # should not have a shebang
     additionalignores=""
     if [[ "$bashfile" == *"completion"* ]]; then
         additionalignores=",SC2148"
     fi
-    scripts/shellcheck -e "$shellcheckignores$additionalignores" "$bashfile"
+    shellcheck -e "$shellcheckignores$additionalignores" "$bashfile"
 done <<< "$bashfiles"
-
-echo "--- Run flake8"
-pythonfiles=$(git grep -El '#!.*python' | grep -v "test.sh")
-
-while read -r pythonfile; do
-    echo "$pythonfile"
-    flake8 "$pythonfile"
-done <<< "$pythonfiles"
