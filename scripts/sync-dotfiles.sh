52e62f1f1d76563d38fb7f7b9b2d9ca3164474ae
Albert Wang <git@albertyw.com>
2021-02-25 22:04:19 -0800

    Supress timesync output when syncing dotfiles

--- scripts/sync-dotfiles.sh
+++ scripts/sync-dotfiles.sh
@@ -48,7 +48,7 @@ trap removelock EXIT
 cd ~/.dotfiles
 
 check_internet
-~/.dotfiles/scripts/time-check.sh
+~/.dotfiles/scripts/time-check.sh &>/dev/null
 update_dotfiles
 
 # Check if there are updates to ssh
