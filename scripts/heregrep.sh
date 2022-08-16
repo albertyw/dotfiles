1dcaa7ddd563c540205b5fdb96ad42208dd63311
Albert Wang <git@albertyw.com>
2022-08-15 20:29:48 -0700

    Add help info on heregrep

--- scripts/heregrep.sh
+++ scripts/heregrep.sh
@@ -6,6 +6,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if [[ -z "${1+x}" ]] || [[ "$1" == "-h" ]]; then
+    echo "Runs a grep operation under each git repository underneath the current pwd"
+    exit
+fi
+
 for f in ./*; do
     if [ ! -d "$f/.git" ]; then
         continue
