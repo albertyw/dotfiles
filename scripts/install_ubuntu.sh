bdd48a8c38cd57607a6f5b36162b0199de9d8d95
Albert Wang <git@albertyw.com>
2022-11-06 21:38:09 -0800

    Add docker to ubuntu install script

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -60,3 +60,11 @@ sudo python3 -m pip install virtualenvwrapper
 sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable
 sudo apt update
 sudo apt install golang
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
