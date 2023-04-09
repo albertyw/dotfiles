68635934041cc8f58b6ee1224ebc9c62234a81f9
Albert Wang <git@albertyw.com>
2023-04-09 13:55:33 -0700

    Fix linting in shellcheck

--- files/completion/npx-completion.sh
+++ files/completion/npx-completion.sh
@@ -1,24 +1,30 @@
-no_modules() {
-	local project_root=$1
+#!/bin/bash
 
-	[ ! -r "$project_root/node_modules" ]
+no_modules() {
+    local project_root
+    project_root=$1
+
+    [ ! -r "$project_root/node_modules" ]
 }
 
 complete_npx() {
-	local project_root=$(pwd -P)
+    local project_root
+    project_root=$(pwd -P)
 
-	while no_modules "$project_root"; do
-		if [ "$project_root" = '/' ]; then
-			return 1
-		fi
+    while no_modules "$project_root"; do
+        if [ "$project_root" = '/' ]; then
+            return 1
+        fi
 
-		project_root=$(dirname "$project_root")
-	done
+        project_root=$(dirname "$project_root")
+    done
 
-	local words=$(find "$project_root/node_modules/.bin" | tail -n +2 | xargs -L 1 -- basename)
-	local current_word=${COMP_WORDS[COMP_CWORD]}
+    local words
+    words=$(find "$project_root/node_modules/.bin" | tail -n +2 | xargs -L 1 -- basename)
+    local current_word=${COMP_WORDS[COMP_CWORD]}
 
-	COMPREPLY=( $(compgen -W "${words[@]}" -- "$current_word" ) )
+    # shellcheck disable=SC2207
+    COMPREPLY=( $(compgen -W "${words[@]}" -- "$current_word" ) )
 }
 
 complete -F complete_npx npx
