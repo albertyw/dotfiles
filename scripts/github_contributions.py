f2f95c03eb5c990478837bd63995ef338da0d248
Albert Wang <git@albertyw.com>
2025-06-28 17:59:45 -0700

    Get local contributions to be pushed

--- scripts/github_contributions.py
+++ scripts/github_contributions.py
@@ -3,6 +3,7 @@
 import datetime
 import json
 import pathlib
+import subprocess
 import urllib.request
 
 
@@ -61,12 +62,46 @@ def get_remote_contributions() -> dict[datetime.date, int]:
     return contributions
 
 
+def get_local_contributions() -> dict[datetime.date, int]:
+    """
+    Returns a dict of local commits to be pushed to Github
+    """
+    git_branch_command = ["git", "branch", "--show-current"]
+    current_branch = subprocess.run(
+        git_branch_command,
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout.strip()
+    contributions: dict[datetime.date, int] = {}
+    if current_branch not in ["master", "main"]:
+       return contributions
+    git_history_command = [
+        "git", "log", "--oneline",
+        "--date=iso", "--pretty=%ad",
+        "origin/%s..%s" % (current_branch, current_branch),
+    ]
+    git_history_output = subprocess.run(
+        git_history_command,
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout.strip()
+    for line in git_history_output.split("\n"):
+        if not line.strip():
+            continue
+        date = datetime.datetime.fromisoformat(line).date()
+        contributions[date] = contributions.get(date, 0) + 1
+    return contributions
+
+
 def main() -> bool:
     """
     Returns whether already pushed plus planned-to-pushed contributions will
     be more than 20 per day
     """
     remote_contributions = get_remote_contributions()
+    get_local_contributions()
     today = datetime.date.today()
     remote_contributions_today = remote_contributions.get(today, 0)
     if remote_contributions_today > 15:
