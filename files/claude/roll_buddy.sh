19f3f8a9fef07d64f8ddba6ff83e02f6702cbb33
Albert Wang <git@albertyw.com>
2026-04-04 22:55:46 -0700

    Add roll_buddy to update claude buddy

---
+++ files/claude/roll_buddy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Generates the claude code buddy
+
+# curl -fsSL https://bun.sh/install | bash
+npx any-buddy@latest \
+    --species owl \
+    --rarity legendary \
+    --eye '✦' \
+    --hat propeller \
+    --shiny yes \
+    --peak WISDOM \
+    --dump CHAOS \
+    --yes
+
+# rm -r ~/.bun
